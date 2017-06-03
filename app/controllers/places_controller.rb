f1117f7745033a76b2ae196065ff7448218939c3
Paulo Sergio <ps131296@gmail.com>
2017-06-03 19:03:40 +0000

    adicionando procura de lugares atraves da Yelp API

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+    require 'yelp_class'
     
     def search_type
     end
@@ -10,6 +11,14 @@ class PlacesController < ApplicationController
         @response = HTTParty.get('http://dados.recife.pe.gov.br' + params[:prev_page])
       else
         @response = HTTParty.get('http://dados.recife.pe.gov.br/api/action/datastore_search?resource_id=d85bf4e3-637e-4e1b-9b03-970dca4403c7')
+      end
+    end
+
+    def search_with_yelp
+      if params[:search_input].nil? or params[:search_input] == ''
+        @response = nil
+      else
+        @response = Yelp.search_by_name(params[:search_input])
       end
     end
     
