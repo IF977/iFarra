de6c2aa18151b01daccf890040c14049f7a1b146
Paulo Sergio <ps131296@gmail.com>
2017-06-17 17:23:48 +0000

    atualizando User para login e cadastro com facebook

--- spec/lib/user_spec.rb
+++ spec/lib/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 require 'User'
 
 describe User do
+    regex_numeros = /[0-9]/
     caracteres_permitidos = /[a-zA-Z0-9]/
     email_valido = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     
@@ -17,8 +18,19 @@ describe User do
         expect(user_instance.email).to be_an_instance_of(String)
         expect(user_instance.email).to match (caracteres_permitidos)
     end
+    
     it "has an valid password?" do
         expect(user_instance.password).not_to be_empty
         expect(user_instance.password).to match (caracteres_permitidos)
+    end
+    
+    it "has an valid provider?" do
+       expect(user_instance.provider).not_to be_empty
+       expect(user_instance.provider).to match (caracteres_permitidos)
+    end
+    
+    it "has an valid uid?" do
+        expect(user_instance.uid).not_to be_empty
+        expect(user_instance.uid).to match(regex_numeros)
     end
 end
